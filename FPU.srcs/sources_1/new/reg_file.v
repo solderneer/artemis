e6dd0c24413d0b4d6a84549fced35746d6072f5e
Sudharshan <sudhar393@gmail.com>
2018-03-02 06:58:09 -0800

    Implement output register assignment
    Needs to be more concise

---
+++ FPU.srcs/sources_1/new/reg_file.v
@@ -0,0 +1,33 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02.03.2018 06:57:14
+// Design Name: 
+// Module Name: reg_file
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module reg_file(
+    input [2:0] I_selA,
+    input [2:0] I_selB,
+    input [2:0] I_selD,
+    input I_clk,
+    input I_en,
+    output [7:0] O_dataA,
+    output [7:0] O_dataB,
+    output [7:0] O_dataD
+    );
+endmodule
