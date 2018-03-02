4701e58a66c15d7b0c4917a5e314f290cab4d0ba
Sudharshan <sudhar393@gmail.com>
2018-03-02 07:39:14 -0800

    Finished source of reg_file

--- FPU.srcs/sources_1/new/reg_file.v
+++ FPU.srcs/sources_1/new/reg_file.v
@@ -24,10 +24,37 @@ module reg_file(
     input [2:0] I_selA,
     input [2:0] I_selB,
     input [2:0] I_selD,
+    input [15:0] I_dataD,
     input I_clk,
     input I_en,
-    output [7:0] O_dataA,
-    output [7:0] O_dataB,
-    output [7:0] O_dataD
+    input I_we,
+    output reg [15:0] O_dataA,
+    output reg [15:0] O_dataB
     );
+    
+    // Initialize internal registers
+    reg [15:0] regs[7:0];
+  
+    // Initialize output registers
+    initial begin
+        O_dataA = 1'd0;
+        O_dataB = 1'd0;
+        
+        for(integer count = 0; count < 8; count = count + 1)
+            regs[count] = 1'd0;
+    end
+    
+    // On clock positive edge
+    always @(posedge I_clk) begin
+        if(I_en)
+        begin
+            // Assign correct output value to A and B
+            O_dataA <= regs[I_selA];
+            O_dataB <= regs[I_selB];
+            
+            // Check for write enable flag and then assign to register
+            if(I_we)
+                regs[I_selD] <= I_dataD;
+        end
+    end
 endmodule
