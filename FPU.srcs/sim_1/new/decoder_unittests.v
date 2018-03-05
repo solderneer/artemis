e16d9576dac905ae9883003f968139c29710f340
Sudharshan <sudhar393@gmail.com>
2018-03-05 14:48:53 +0800

    Added instruction decoder and its unit test

---
+++ FPU.srcs/sim_1/new/decoder_unittests.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05.03.2018 14:45:47
+// Design Name: 
+// Module Name: decoder_unittests
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module decoder_unittests(
+
+    );
+endmodule
