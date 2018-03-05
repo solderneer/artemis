3bd0a134931b6c4cf757a691a7dd83d3cf6f9b2c
Sudharshan <sudhar393@gmail.com>
2018-03-05 21:53:47 +0800

    Finished inst_decoder unit tests and tests passed

--- FPU.sim/sim_1/behav/compile.bat
+++ FPU.sim/sim_1/behav/compile.bat
@@ -1,7 +1,7 @@
 @echo off
 set xv_path=C:\\Xilinx\\Vivado\\2016.2\\bin
-echo "xvlog -m64 --relax -prj regfile_unittest_vlog.prj"
-call %xv_path%/xvlog  -m64 --relax -prj regfile_unittest_vlog.prj -log xvlog.log
+echo "xvlog -m64 --relax -prj decoder_unittests_vlog.prj"
+call %xv_path%/xvlog  -m64 --relax -prj decoder_unittests_vlog.prj -log xvlog.log
 call type xvlog.log > compile.log
 if "%errorlevel%"=="1" goto END
 if "%errorlevel%"=="0" goto SUCCESS
--- FPU.srcs/sim_1/new/decoder_unittests.v
+++ FPU.srcs/sim_1/new/decoder_unittests.v
@@ -23,4 +23,49 @@
 module decoder_unittests(
 
     );
+    
+    reg [15:0] I_inst;
+    reg I_clk;
+    reg I_en;
+    
+    wire [4:0] O_aluop;
+    wire [15:0] O_Imm;
+    wire [3:0] O_selA;
+    wire [3:0] O_selB;
+    wire [3:0] O_selD;
+    wire O_regwe;
+    
+    inst_decoder unittest  (I_inst, 
+                            I_clk, 
+                            I_en, 
+                            O_aluop, 
+                            O_Imm, 
+                            O_selA, 
+                            O_selB, 
+                            O_selD);
+    
+    /* Procedure for testing
+     * 1) Insert proper instruction
+     * 2) Check update for proper slicing after 1 clk cycle
+     */
+     
+    initial begin
+        // Initialize intial values for regs
+        I_inst = 0;
+        I_en = 0;
+        I_clk = 0;
+        
+        #10;
+        I_inst = 16'b0001011100000100; // add r7 r1,r2
+        
+        #10;
+        I_en = 1;
+    end
+    
+    always begin
+        #5; I_clk = ~I_clk;
+    end
+
+                            
+    
 endmodule
