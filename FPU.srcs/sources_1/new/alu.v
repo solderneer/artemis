1dca8876000432d45899ae33893c737b13140f06
Sudharshan <sudhar393@gmail.com>
2018-03-14 13:38:37 +0800

    Added signed operations into ALU

--- FPU.srcs/sources_1/new/alu.v
+++ FPU.srcs/sources_1/new/alu.v
@@ -44,57 +44,65 @@ module alu(
     always @(posedge I_clk) begin
         case(I_aluop[4:1])
             (4'b0000) : begin
-                        int_Result <= I_dataA + I_dataB;
-                        O_shldBranch <= 0;
+                            int_Result <= (I_aluop[0]) ? ($signed(I_dataA) + $signed(I_dataB)) : (I_dataA + I_dataB);
+                            O_shldBranch <= 0;
                         end
             (4'b0001) : begin 
-                        int_Result <= I_dataA - I_dataB;
-                        O_shldBranch <= 0;
+                            int_Result <= (I_aluop[0]) ? ($signed(I_dataA) - $signed(I_dataB)) : (I_dataA - I_dataB);
+                            O_shldBranch <= 0;
                         end
             (4'b0010) : begin 
-                        int_Result <= I_dataA | I_dataB;
-                        O_shldBranch <= 0;
+                            int_Result <= I_dataA | I_dataB;
+                            O_shldBranch <= 0;
                         end
             (4'b0011) : begin 
-                        int_Result <= I_dataA & I_dataB;
-                        O_shldBranch <= 0;
+                            int_Result <= I_dataA & I_dataB;
+                            O_shldBranch <= 0;
                         end
             (4'b0100) : begin 
-                        int_Result <= I_dataA ^ I_dataB;
-                        O_shldBranch <= 0;
+                            int_Result <= I_dataA ^ I_dataB;
+                            O_shldBranch <= 0;
                         end
             (4'b0101) : begin 
-                        int_Result <= ~I_dataA;
-                        O_shldBranch <= 0;
+                            int_Result <= ~I_dataA;
+                            O_shldBranch <= 0;
                         end 
             (4'b1000) : begin
-                        int_Result <= (I_aluop[0]) ? {I_imm, 8'hXX} : {8'hXX, I_imm};
-                        O_shldBranch <= 0;
+                            int_Result <= (I_aluop[0]) ? {I_imm, 8'hXX} : {8'hXX, I_imm};
+                            O_shldBranch <= 0;
                         end
             (4'b1001) : begin
-                        int_Result[0] <= (I_dataA == I_dataB) ? 1 : 0;
-                        int_Result[1] <= (I_dataA == 0) ? 1 : 0;
-                        int_Result[2] <= (I_dataB == 0) ? 1 : 0;
-                        int_Result[3] <= (I_dataA > I_dataB) ? 1 : 0;
-                        int_Result[4] <= (I_dataA < I_dataB) ? 1 : 0;
-                        
-                        O_shldBranch <= 0;
+                            if (I_aluop[0]) begin
+                                int_Result[0] <= ($signed(I_dataA) == $signed(I_dataB)) ? 1 : 0;
+                                int_Result[1] <= ($signed(I_dataA) == 0) ? 1 : 0;
+                                int_Result[2] <= ($signed(I_dataB) == 0) ? 1 : 0;
+                                int_Result[3] <= ($signed(I_dataA) > $signed(I_dataB)) ? 1 : 0;
+                                int_Result[4] <= ($signed(I_dataA) < $signed(I_dataB)) ? 1 : 0;
+                            end
+                            else begin
+                                int_Result[0] <= (I_dataA == I_dataB) ? 1 : 0;
+                                int_Result[1] <= (I_dataA == 0) ? 1 : 0;
+                                int_Result[2] <= (I_dataB == 0) ? 1 : 0;
+                                int_Result[3] <= (I_dataA > I_dataB) ? 1 : 0;
+                                int_Result[4] <= (I_dataA < I_dataB) ? 1 : 0;
+                            end
+                            O_shldBranch <= 0;
                         end
             (4'b1010) : begin
-                        int_Result <= I_dataA << (I_dataB[3:0]);
-                        O_shldBranch <= 0;
+                            int_Result <= I_dataA << (I_dataB[3:0]);
+                            O_shldBranch <= 0;
                         end
             (4'b1011) : begin
-                        int_Result <= I_dataA >> (I_dataB[3:0]);
-                        O_shldBranch <= 0;
+                            int_Result <= I_dataA >> (I_dataB[3:0]);
+                            O_shldBranch <= 0;
                         end
             (4'b1100) : begin
-                        int_Result <= (I_aluop[0]) ? I_dataA : I_imm;
-                        O_shldBranch <= 1;
+                            int_Result <= (I_aluop[0]) ? I_dataA : I_imm;
+                            O_shldBranch <= 1;
                         end
             (4'b1101) : begin
-                        int_Result <= I_dataB;
-                        O_shldBranch <= I_dataB[{I_aluop[0], I_imm[1:0]}];
+                            int_Result <= I_dataB;
+                            O_shldBranch <= I_dataB[{I_aluop[0], I_imm[1:0]}];
                         end
         endcase
     end
